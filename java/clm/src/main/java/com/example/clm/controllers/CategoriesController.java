3a3941eb470012bbb0565206806db11d6dd9a2ce
Mazene <mmazenezerguine@gmail.com>
2023-03-22 17:59:50 +0100

    [client] feat: geting all categories interface

--- java/clm/src/main/java/com/example/clm/controllers/CategoriesController.java
+++ java/clm/src/main/java/com/example/clm/controllers/CategoriesController.java
@@ -1,14 +1,34 @@
 package com.example.clm.controllers;
 
 import com.example.clm.Main;
+import com.example.clm.models.Categorie;
+import com.example.clm.utils.ApiService;
+import com.github.tsohr.JSONArray;
+import com.github.tsohr.JSONObject;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public class CategoriesController extends Application {
+
+public class CategoriesController extends Application implements Initializable {
+
+	@FXML
+	private ListView<String> categoriesListView ;
+
+	private List<Categorie> categories = new ArrayList<Categorie>() ;
 	@Override
 	public void start(Stage stage) throws IOException {
 		FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("templates/categories-view.fxml"));
@@ -21,5 +41,32 @@ public class CategoriesController extends Application {
 
 	public static void main(String[] args) {
 		launch();
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle resourceBundle) {
+		StringBuilder response = new StringBuilder() ;
+		ApiService api = new ApiService() ;
+		try {
+		response =	api.getTypeRequest("http://localhost:3000/api/categories");
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		JSONObject json = new JSONObject(response.toString());
+
+		JSONArray data = json.getJSONArray("categories") ;
+
+		for (int i = 0 ; i < data.length() ; i++) {
+			Categorie categorie = new Categorie(
+				data.getJSONObject(i).getInt("id"),
+				data.getJSONObject(i).getString("title"),
+				data.getJSONObject(i).getString("disciption")
+			) ;
+			categories.add(categorie);
+
+		}
+		for (Categorie item : categories) {
+			categoriesListView.getItems().addAll(item.getTitle());
+		}
 	}
 }
