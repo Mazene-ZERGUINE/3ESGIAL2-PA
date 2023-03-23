b493863c463721d34007fafa5f5335639dc9219a
Mazene <mmazenezerguine@gmail.com>
2023-03-23 16:43:38 +0100

    [client] feat: adding new categories and deleting categories

--- java/clm/src/main/java/com/example/clm/controllers/CategoriesController.java
+++ java/clm/src/main/java/com/example/clm/controllers/CategoriesController.java
@@ -6,19 +6,21 @@ import com.example.clm.utils.ApiService;
 import com.github.tsohr.JSONArray;
 import com.github.tsohr.JSONObject;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -28,7 +30,17 @@ public class CategoriesController extends Application implements Initializable {
 	@FXML
 	private ListView<String> categoriesListView ;
 
-	private List<Categorie> categories = new ArrayList<Categorie>() ;
+	private final ApiService api = new ApiService() ;
+	private final String baseUrl = "http://localhost:3000/api/" ;
+	@FXML
+	private Button deleteBtn;
+	private List<Categorie> categories = new ArrayList<>() ;
+	@FXML
+	private TextArea categorieDescription;
+	@FXML
+	private Button addBtn;
+	@FXML
+	private TextField categorieTitle;
 	@Override
 	public void start(Stage stage) throws IOException {
 		FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("templates/categories-view.fxml"));
@@ -45,10 +57,32 @@ public class CategoriesController extends Application implements Initializable {
 
 	@Override
 	public void initialize(URL url, ResourceBundle resourceBundle) {
+	getAllCategories();
+	}
+
+	@FXML
+	void onDeleteBtnClick(ActionEvent event) throws IOException {
+		String selectedElement = categoriesListView.getSelectionModel().getSelectedItems().get(0);
+
+	 List<Categorie> selectedCategorie = categories.stream().filter(e -> Objects.equals(e.getTitle(), selectedElement)).toList();
+	 for (Categorie element : selectedCategorie) {
+		 try {
+			 StringBuilder response = new StringBuilder();
+			 response = api.deleteTypeRequest(baseUrl + "categories/" + element.getId());
+			 JSONObject json = new JSONObject(response.toString());
+			 if (json.getInt("status_code") == 200) {
+					categoriesListView.getItems().remove(selectedElement);
+			 }
+		 } catch (IOException e) {
+			 System.out.println("error ocured while sending http delete request" + e);
+		 }
+	 }
+	}
+
+	private void getAllCategories() {
 		StringBuilder response = new StringBuilder() ;
-		ApiService api = new ApiService() ;
 		try {
-		response =	api.getTypeRequest("http://localhost:3000/api/categories");
+			response =	api.getTypeRequest(baseUrl + "categories");
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
@@ -60,7 +94,7 @@ public class CategoriesController extends Application implements Initializable {
 			Categorie categorie = new Categorie(
 				data.getJSONObject(i).getInt("id"),
 				data.getJSONObject(i).getString("title"),
-				data.getJSONObject(i).getString("disciption")
+				data.getJSONObject(i).getString("desciption")
 			) ;
 			categories.add(categorie);
 
@@ -69,4 +103,35 @@ public class CategoriesController extends Application implements Initializable {
 			categoriesListView.getItems().addAll(item.getTitle());
 		}
 	}
+
+	@FXML
+	void onCategorieListSelected(MouseEvent event) {
+		String selectedElement = categoriesListView.getSelectionModel().getSelectedItems().get(0);
+		List<Categorie> selectedCategorie = categories.stream().filter(e -> Objects.equals(e.getTitle(), selectedElement)).toList();
+
+		for (Categorie element : selectedCategorie) {
+			categorieTitle.setText(element.getTitle());
+			categorieDescription.setText(element.getDiscreption());
+		}
+	}
+
+	@FXML
+	void onaddBtnClick(ActionEvent event) throws IOException {
+		String title = categorieTitle.getText().toString();
+		String description = categorieDescription.getText().toString() ;
+
+
+		JSONObject data = new JSONObject() ;
+			data.put("title" , title) ;
+		  data.put("desciption" , description) ;
+			StringBuilder response = new StringBuilder() ;
+			response = api.postTypeRequest(baseUrl + "categories/" , data) ;
+			JSONObject json = new JSONObject(response.toString());
+			if (json.getInt("status_code") == 200) {
+				categoriesListView.getItems().clear();
+				categories.clear();
+				getAllCategories();
+			}
+	}
+
 }
