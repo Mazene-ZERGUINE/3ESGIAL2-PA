42e5e697416d2b500c17b6adf66e990a1e2d4be5
Mazene <mmazenezerguine@gmail.com>
2023-03-25 14:23:46 +0100

    [client]feat: updating sceneService to handel sending data between controllers

--- java/clm/src/main/java/com/example/clm/utils/SceneService.java
+++ java/clm/src/main/java/com/example/clm/utils/SceneService.java
@@ -6,27 +6,35 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class SceneService {
 
 	public SceneService() { }
 
-	public void switchScene(Stage stage , String fileName) {
+	public void switchScene(Stage stage , String fileName , Parent root) throws IOException {
 		FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("templates/" + fileName));
+		if (root == null) {
+			root = fxmlLoader.load() ;
+ 		}
 		try {
-			Parent root = fxmlLoader.load();
 			stage.setScene(new Scene(root));
+			stage.setResizable(false);
 			stage.show();
 		} catch (Exception exception) {
 			System.out.println("can't change current scene");
 		}
 	}
 
-	public void switchToNewWindow(String fileName) {
+	public void switchToNewWindow(String fileName , Parent root) throws IOException {
 		FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("templates/" + fileName));
+		if (root == null) {
+			root = fxmlLoader.load() ;
+		}
 		try {
-			Parent root = fxmlLoader.load();
 			Stage stage = new Stage();
 			stage.setScene(new Scene(root));
+			stage.setResizable(false);
 			stage.show();
 		} catch (Exception exception) {
 			System.out.println("can't change current scene");
