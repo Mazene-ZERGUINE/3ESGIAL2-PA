10a2804e9497507b86db309704f56aa114a14ce2
LoicZHU <zhu.loic@gmail.com>
2023-04-21 22:58:00 +0200

    [backend] fix(trello): update model

--- backend/src/db/client/client_user.sql
+++ backend/src/db/client/client_user.sql
@@ -4,42 +4,11 @@ CREATE TABLE client_user (
 	id SERIAL PRIMARY KEY,
 	email VARCHAR NOT NULL,
 	password VARCHAR NOT NULL,
-	firstName VARCHAR NOT NULL,
-	lastName VARCHAR NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
 	created_at DATE NOT NULL,
-	updated_at DATE
-);
-
-INSERT INTO client_user (
-  email,
-  password,
-  firstName,
-  lastName,
-  created_at,
-  updated_at
-) VALUES (
-  'q1@email.fr',
-  'password1',
-  'firstName1',
-  'lastName1',
-  current_timestamp,
-  NULL
-);
-
-INSERT INTO client_user (
-  email,
-  password,
-  firstName,
-  lastName,
-  created_at,
-  updated_at
-) VALUES (
-  'q2@email.fr',
-  'password2',
-  'firstName2',
-  'lastName2',
-  current_timestamp,
-  NULL
+	updated_at DATE,
+	UNIQUE (email)
 );
 
 SELECT * FROM client_user;
