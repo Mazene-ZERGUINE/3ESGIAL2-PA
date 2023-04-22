28acc9fbcd0534491f2acae6ecb2245dd1f47ffd
LoicZHU <zhu.loic@gmail.com>
2023-04-22 21:32:58 +0200

    [backend] feat(trello): add password hashing on sign in

--- backend/src/controllers/client/sign-in.controller.ts
+++ backend/src/controllers/client/sign-in.controller.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from 'express';
+import { verify } from 'argon2';
+
 import { signInQuery } from '../../queries/client/sign-in-queries';
+import { getOneBy } from '../../utils/crud';
 
 const clientPool: any = require('../../db/clientPool');
 
@@ -19,17 +22,26 @@ const signIn = (req: Request, res: Response) => {
 			return;
 		}
 
-		clientPool.query(signInQuery.findUserWithEmailAndPassword(), [email, password], (error: Error, results: any) => {
+		clientPool.query(getOneBy('client_user', ['email']), [email], async (error: Error, results: any) => {
 			if (error) {
 				res.status(500).send('Internal server error' + error);
 				return;
 			}
 
-			isUserFound = results.rows[0]?.count >= 1;
-			if (!isUserFound) {
-				res.status(400).json({ error: 'Vos identifiants sont incorrects.' });
-			} else {
+			const userPassword = results.rows[0].password;
+			let isPasswordVerified = false;
+
+			try {
+				isPasswordVerified = await verify(userPassword, password);
+				if (!isPasswordVerified) {
+					res.status(400).json({ error: 'Vos identifiants sont incorrects.' });
+					return;
+				}
+
 				res.status(204).end();
+				return;
+			} catch (e) {
+				res.status(500).send('Internal server error ' + error);
 			}
 		});
 	});
