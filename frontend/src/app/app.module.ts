dd656e37de414764287663a000a889e7332f0ce7
LoicZHU <zhu.loic@gmail.com>
2023-05-04 19:46:22 +0200

    [UI] feat: handle uppercase URLs

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ComponentsModule } from './shared/components/components.module';
+import { DefaultUrlSerializer, UrlSerializer, UrlTree } from '@angular/router';
+
+export class LowerCaseUrlSerializer extends DefaultUrlSerializer {
+  override parse(url: string): UrlTree {
+    return super.parse(url.toLowerCase());
+  }
+}
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, NgbModule, AppRoutingModule, ComponentsModule],
-  providers: [],
+  providers: [
+    {
+      provide: UrlSerializer,
+      useClass: LowerCaseUrlSerializer,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
