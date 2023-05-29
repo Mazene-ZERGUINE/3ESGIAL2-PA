ce037f168121e16ba903ca77f105f85d6241f5b0
LoicZHU <zhu.loic@gmail.com>
2023-05-29 17:31:19 +0200

    [client] feat(header): add temporary UI (logo & routes)

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -5,4 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  routes: ReadonlyArray<{ path: string; label: string }> = [
+    { path: '/administration', label: 'Administration' },
+    { path: '/', label: 'Objets' },
+    { path: '/chat', label: 'Messagerie' },
+    { path: '/users', label: 'Profil' },
+    { path: '/', label: 'Déconnexion' },
+  ];
+}
