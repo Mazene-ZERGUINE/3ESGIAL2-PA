849b3172d55fb5cc4ddf0eefe31dcae0e80e0daf
LoicZHU <zhu.loic@gmail.com>
2023-05-29 14:09:41 +0200

    [UI] feat(app): update HTML, handle header & footer

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'frontend';
+  hideHeader = false;
+  hideFooter = false;
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.subscribeToRouterEvents();
+  }
+
+  private subscribeToRouterEvents(): void {
+    this.router.events
+      .pipe(
+        filter((event) => {
+          return event instanceof NavigationEnd;
+        }),
+      )
+      .subscribe(() => {
+        this.hideHeader = this.activatedRoute.firstChild?.snapshot.data?.['hideHeader'] ?? false;
+        this.hideFooter = this.activatedRoute.firstChild?.snapshot.data?.['hideFooter'] ?? false;
+      });
+  }
 }
