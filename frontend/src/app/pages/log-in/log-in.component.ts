71305c6bcbb5be44347ad3e35e9dcc40f811620e
LoicZHU <zhu.loic@gmail.com>
2023-05-29 16:00:15 +0200

    [UI] feat(log-in): add UI

--- frontend/src/app/pages/log-in/log-in.component.ts
+++ frontend/src/app/pages/log-in/log-in.component.ts
@@ -1,8 +1,40 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UntilDestroy } from '@ngneat/until-destroy';
 
+import { AuthService } from '../../shared/services/core/auth/auth.service';
+
+@UntilDestroy()
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
   styleUrls: ['./log-in.component.scss'],
 })
-export class LogInComponent {}
+export class LogInComponent {
+  form?: FormGroup;
+
+  constructor(private readonly authService: AuthService, private readonly fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(): void {
+    this.form = this.fb.group({
+      email: this.fb.control('', [Validators.required, Validators.email]),
+      password: this.fb.control('', Validators.required),
+    });
+  }
+
+  onSubmit(): void {
+    if (this.form?.invalid) {
+      return;
+    }
+
+    this.logIn();
+  }
+
+  logIn(): void {
+    // TODO
+  }
+}
--- frontend/src/app/pages/log-in/log-in.module.ts
+++ frontend/src/app/pages/log-in/log-in.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { LogInRoutingModule } from './log-in-routing.module';
 import { LogInComponent } from './log-in.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LogInComponent],
-  imports: [CommonModule, LogInRoutingModule],
+  imports: [CommonModule, LogInRoutingModule, ReactiveFormsModule],
 })
 export class LogInModule {}
