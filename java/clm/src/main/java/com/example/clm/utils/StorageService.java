03e2a7bb42c879c388086d427bbdc326a25ecb75
Mazene <mmazenezerguine@gmail.com>
2023-06-13 21:57:39 +0200

    client: adding storage service

--- java/clm/src/main/java/com/example/clm/utils/StorageService.java
+++ java/clm/src/main/java/com/example/clm/utils/StorageService.java
@@ -1,2 +1,33 @@
-package com.example.clm.utils;public class StorageService {
+package com.example.clm.utils;
+
+public class StorageService {
+
+	private String selectedExportFormat ;
+	private static StorageService instance;
+
+	private String exportFileName;
+	private StorageService() {}
+
+	public static StorageService getInstance() {
+		if (instance == null) {
+			instance = new StorageService();
+		}
+		return instance;
+	}
+
+	public String getSelectedExportFormat() {
+		return selectedExportFormat;
+	}
+
+	public void setSelectedExportFormat(String selectedExportFormat) {
+		this.selectedExportFormat = selectedExportFormat;
+	}
+
+	public void setExportFileName(String exportFileName) {
+			this.exportFileName = exportFileName ;
+	}
+
+	public String getExportFileName() {
+		return this.exportFileName;
+	}
 }
