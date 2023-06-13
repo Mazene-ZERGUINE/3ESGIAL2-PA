cc0ad0091453366d9c02a1ef73a6ad43893cadaf
Mazene <mmazenezerguine@gmail.com>
2023-06-13 21:57:21 +0200

    client: adding export controller

--- java/clm/src/main/java/com/example/clm/controllers/ExportFileNameController.java
+++ java/clm/src/main/java/com/example/clm/controllers/ExportFileNameController.java
@@ -1,2 +1,29 @@
-package com.example.clm.controllers;public class ExportFileNameController {
+package com.example.clm.controllers;
+
+import com.example.clm.utils.NotifierService;
+import com.example.clm.utils.StorageService;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+import tray.notification.NotificationType;
+
+public class ExportFileNameController {
+	@FXML
+	private TextArea fileNameTextArea;
+	@FXML private Button saveBtn;
+
+	@FXML
+	private void onSaveBtnClick(ActionEvent event) {
+		String fileName = this.fileNameTextArea.getText();
+		if (fileName.trim().isEmpty()) {
+			NotifierService notifierService = new NotifierService();
+			notifierService.notify(NotificationType.ERROR , "Erreur" , "Nom de fichier est obligatoir") ;
+			return;
+		}
+		StorageService.getInstance().setExportFileName(fileName);
+		Stage stage = (Stage) this.saveBtn.getScene().getWindow();
+		stage.close();
+	}
 }
--- java/clm/src/main/java/com/example/clm/controllers/ExportsController.java
+++ java/clm/src/main/java/com/example/clm/controllers/ExportsController.java
@@ -1,2 +1,83 @@
-package com.example.clm.controllers;public class ExportsController {
+package com.example.clm.controllers;
+
+import com.example.clm.utils.NotifierService;
+import com.example.clm.utils.StorageService;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventType;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import tray.notification.NotificationType;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class ExportsController implements Initializable {
+	@FXML
+	private HBox formatBox;
+	public String selectedFormat;
+
+
+
+
+private static NotifierService notifier = new NotifierService();
+	@Override
+	public void initialize(URL url, ResourceBundle resourceBundle) {
+
+		File folder = new File("../plugins");
+		File[] subfolders = folder.listFiles(File::isDirectory);
+
+		if (subfolders != null) {
+			for (File subfolder : subfolders) {
+				loadAndDisplayImagesFromFolder(this.formatBox , subfolder.getName());
+			}
+		} else {
+				notifier.notify(NotificationType.WARNING , "Avertisement" , "Auccune format d'éxport n'est enregistrer pour le moment");
+				return;
+		}
+	}
+
+
+
+	private void loadAndDisplayImagesFromFolder(HBox hbox, String folderName) {
+		File file = new File("../plugins/" + folderName + "/" + folderName+".png");
+		if (file != null) {
+
+			try {
+						FileInputStream fileInputStream = new FileInputStream(file);
+						Image image = new Image(fileInputStream);
+						ImageView imageView = new ImageView(image);
+
+						imageView.setFitWidth(80);
+						imageView.setFitHeight(60);
+
+						formatBox.getChildren().add(imageView);
+						formatBox.setPadding( new Insets(25));
+
+						imageView.setOnMouseClicked(event -> {
+							this.selectedFormat = file.getName().split("\\.")[0] ;
+							StorageService.getInstance().setSelectedExportFormat(selectedFormat);
+							Stage stage = (Stage) hbox.getScene().getWindow();
+							stage.close();
+						});
+
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				} else {
+			System.out.println("filde do not exist");
+		}
+	}
+
 }
