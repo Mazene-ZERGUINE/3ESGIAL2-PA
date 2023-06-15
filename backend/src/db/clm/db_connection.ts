c96941f265c3ad578995b4d1560d961b1ce43826
LoicZHU <zhu.loic@gmail.com>
2023-06-16 00:16:54 +0200

    [API] chore(sequelize): add configuration

---
+++ backend/src/db/clm/db_connection.ts
@@ -0,0 +1,13 @@
+import { Dialect } from 'sequelize';
+import { Sequelize } from 'sequelize-typescript';
+
+export const sequelize = new Sequelize({
+	database: process.env.DB_NAME as string,
+	dialect: process.env.DB_DIALECT as Dialect,
+	username: process.env.DB_USER as string,
+	password: process.env.DB_PASSWORD as string,
+	host: process.env.DB_HOST as string,
+	port: parseInt(process.env.DB_PORT as string),
+	models: [__dirname + '/models/clm'],
+	logging: false,
+});
