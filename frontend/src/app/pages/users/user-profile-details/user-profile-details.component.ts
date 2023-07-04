3ec89842deab6d31ce0884e4fb0663814711393a
LoicZHU <zhu.loic@gmail.com>
2023-07-04 22:27:53 +0200

    [UI] feat: add user's publications count

--- frontend/src/app/pages/users/user-profile-details/user-profile-details.component.ts
+++ frontend/src/app/pages/users/user-profile-details/user-profile-details.component.ts
@@ -6,8 +6,10 @@ import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { ModalReportComponent } from '../../../shared/components/modal-report/modal-report.component';
 import { UserProfileService } from '../shared/services/user-profile/user-profile.service';
 import { User } from '../../../shared/core/models/interfaces/user.interface';
-import { map } from 'rxjs';
+import { map, switchMap, tap } from 'rxjs';
 import { Response } from '../../../shared/core/models/interfaces/response.interface';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { UserPostsService } from '../shared/services/user-posts/user-posts.service';
 
 @UntilDestroy()
 @Component({
@@ -18,16 +20,19 @@ import { Response } from '../../../shared/core/models/interfaces/response.interf
 export class UserProfileDetailsComponent implements OnInit {
   usernameParam: string;
   user?: User;
+  postsCount = 0;
 
   constructor(
+    private readonly jwtHelper: JwtHelperService,
     private readonly modalService: NgbModal,
     private readonly route: ActivatedRoute,
+    private readonly userPostsService: UserPostsService,
     private readonly userProfileService: UserProfileService,
   ) {
     this.usernameParam = this.route.snapshot.paramMap.get('username') || '';
   }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     // TODO utilisateur inexistant
     this.getUser();
   }
@@ -41,14 +46,15 @@ export class UserProfileDetailsComponent implements OnInit {
   }
 
   getUser() {
-    this.userProfileService
-      .getOneByField<Response<User>>('utilisateurs', this.usernameParam)
+    this.userPostsService
+      .count<Response<number>>(`utilisateurs/${this.usernameParam}/publications/count/public`)
       .pipe(
+        tap((res) => (this.postsCount = res.data)),
+        switchMap((_) => this.userProfileService.getOneByField<Response<User>>('utilisateurs', this.usernameParam)),
         map((res) => res?.data),
         untilDestroyed(this),
       )
       .subscribe((data) => {
-        console.log('sub getuser', data);
         this.user = data;
       });
   }
