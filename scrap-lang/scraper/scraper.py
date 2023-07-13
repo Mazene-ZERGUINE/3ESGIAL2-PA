c0f2edeff700903bb4bdd304f3e34a8c4968e4f4
Mazene <mmazenezerguine@gmail.com>
2023-07-13 19:01:31 +0200

    scrap-lang: improving fetchAll

--- scrap-lang/scraper/scraper.py
+++ scrap-lang/scraper/scraper.py
@@ -23,24 +23,40 @@ def scrap_table(content: BeautifulSoup , table_id):
         raise Exception("the table with the id " , table_id ," is not found")
     return table 
 
+def fetch_all(data: BeautifulSoup):
+    if isinstance(data, list):
+        return [header.text.strip() for header in data if header.name.startswith('h')]
+    
+    elif data.name == 'table':
+        table_data = []
+        
+        header_rows = data.find_all('th')
+        headers = [header.text.strip() for header in header_rows]
+        
+        body_rows = data.find('tbody').find_all('tr') if data.find('tbody') else data.find_all('tr')
+        
+        for row in body_rows:
+            row_data = {}
+            cells = row.find_all('td')
+            for index, cell in enumerate(cells):
+                header = headers[index]
+                cell_text = cell.text.strip()
+                row_data[header] = cell_text
+            table_data.append(row_data)
 
-def fetch_all(table: BeautifulSoup):
+        return table_data
     
-    table_data = []
-    
-    rows = table.find('tbody').find_all('tr') if table.find('tbody') else table.find_all('tr')
-    
-    for row in rows:
-        row_data = [cell.text.strip() for cell in row.find_all('td')]
-        table_data.append(row_data)
+    else:
+        return None
 
-    return table_data
+
+
+
     
 
 def get_page_titles(html_content: BeautifulSoup):
     heading_tags = html_content.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
-    headings = [tag.get_text() for tag in heading_tags]
-    return headings
+    return heading_tags
 
 
 def filter_data(content: BeautifulSoup , heading):
@@ -48,4 +64,3 @@ def filter_data(content: BeautifulSoup , heading):
     heading_tag = heading.replace('"' , "")
     data = content.find_all(heading_tag)
     
-    print(data)
